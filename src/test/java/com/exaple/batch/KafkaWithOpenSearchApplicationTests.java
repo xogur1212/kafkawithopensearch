840639babfc2e5a7611bea169935f0c8ba01aaf2
xogur12 <xogur1212@naver.com>
2023-10-14 16:50:28 +0900

    23/10/14 카프카 세팅 및 오픈서치 설치

---
+++ src/test/java/com/exaple/batch/KafkaWithOpenSearchApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.kafkawithopensearch;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class KafkaWithOpenSearchApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
